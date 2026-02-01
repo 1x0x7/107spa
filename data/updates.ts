2f07d4650d73ae04e1a461f0661cb9b0e734ad62
tjdks <seoulcandlelong@gmail.com>
2026-02-01 21:33:22 +0900

    farming update

--- data/updates.ts
+++ data/updates.ts
@@ -14,7 +14,7 @@ export const UPDATE_HISTORY: UpdateItem[] = [
   { 
     title: "재배 업데이트", 
     desc: "요리가격 변동", 
-    date: "2025.01.30", 
+    date: "2025.02.01", 
     isLatest: true,
     details: {
       changes: [
@@ -23,11 +23,11 @@ export const UPDATE_HISTORY: UpdateItem[] = [
       fixes: [
       ],
       notes: [
-        "다음 업데이트 : 02.01 예정"
+        "다음 업데이트 : 02.03 예정"
       ]
     }
   },
+  { title: "재배 업데이트", date: "2025.01.30" },
   { title: "정기 점검 업데이트", date: "2025.01.28" },
   { title: "요리 가격 업데이트", date: "2025.01.27" },
-  { title: "해양 수정 및 사이트 리뉴얼", date: "2025.01.25" },
 ]
