e6f8bd38c5817119a2e9976f5bc047c719c34304
tjdks <seoulcandlelong@gmail.com>
2026-02-18 03:02:15 +0900

    farming update

--- data/updates.ts
+++ data/updates.ts
@@ -13,25 +13,23 @@ export interface UpdateItem {
 export const UPDATE_HISTORY: UpdateItem[] = [
   { 
     title: "요리 가격 업데이트", 
-    desc: "소갈비 꼬치 > 삼겹살 토마토 찌개 > 토마토 라자냐", 
-    date: "2025.02.15", 
+    desc: "소갈비 꼬치 > 로스트 치킨 파이 > 달콤 시리얼", 
+    date: "2025.02.18", 
     isLatest: true,
     details: {
       changes: [
         "요리 가격 변동",
-        "사냥꾼 스태미나 페이지 추가",
-        "골드 이미지 추가",
       ],
       fixes: [
 
 
       ],
       notes: [
-        "다음 업데이트 : 02.18 예정(정기 업데이트)"
+        "다음 업데이트 : 02.21 예정(정기 업데이트)"
       ]
     }
   },
+  { title: "요리가격 업데이트", date: "2025.02.15" },
   { title: "사냥 전문가, 각인 시스템 추가", date: "2025.02.05" },
   { title: "강화 / 수수료 계산 추가", date: "2025.02.03" },
-  { title: "재배/해양 업데이트", date: "2025.02.03" },
 ]
