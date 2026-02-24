4a58c9f053733126e6eb13b2084f84edda8ceed2
tjdks <seoulcandlelong@gmail.com>
2026-02-24 09:08:45 +0900

    요리 가격 수정

--- data/updates.ts
+++ data/updates.ts
@@ -13,24 +13,22 @@ export interface UpdateItem {
 export const UPDATE_HISTORY: UpdateItem[] = [
   { 
     title: "요리 가격 업데이트", 
-    desc: "삼색 아이스크림 > 로스트 치킨 파이 > 딥 크림 빠네", 
-    date: "2025.02.21", 
+    desc: "달콤 시리얼 > 토마토 라자냐 > 허브 삼겹살 찜", 
+    date: "2025.02.24", 
     isLatest: true,
     details: {
       changes: [
         "요리 가격 변동",
-        "해양 재료 가격 입력 가능",
       ],
       fixes: [
-        "다크모드 수정",
 
       ],
       notes: [
-        "다음 업데이트 : 02.24 예정(요리 가격변동)"
+        "다음 업데이트 : 02.27 예정(요리 가격변동)"
       ]
     }
   },
+  { title: "요리가격 업데이트", date: "2025.02.24" },
   { title: "요리가격 업데이트", date: "2025.02.21" },
   { title: "요리가격 업데이트", date: "2025.02.15" },
-  { title: "사냥 전문가, 각인 시스템 추가", date: "2025.02.05" },
 ]
