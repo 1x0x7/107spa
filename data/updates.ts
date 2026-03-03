cde7145b8c01240bf722a8c5602a2c8657396afb
1x0x7 <seoulcandlelong@gmail.com>
2026-03-03 16:55:21 +0900

    요리 가격 변동

--- data/updates.ts
+++ data/updates.ts
@@ -12,13 +12,12 @@ export interface UpdateItem {
 
 export const UPDATE_HISTORY: UpdateItem[] = [
   { 
-    title: "요리 업데이트!!", 
-    desc: "트리플 소갈비 꼬치 > 딥 크림 빠네 > 허브 삼겹살 찜 ", 
-    date: "2025.03.01", 
+    title: "요리 업데이트", 
+    desc: "로스트 치킨 파이 > 스윗 치킨 햄버거 > 허브 삼겹살 찜 ", 
+    date: "2025.03.03", 
     isLatest: true,
     details: {
       changes: [
-        "강화, 계약서 가격 변경"
       ],
       fixes: [
 
@@ -28,8 +27,8 @@ export const UPDATE_HISTORY: UpdateItem[] = [
       ]
     }
   },
+  { title: "요리가격 업데이트", date: "2025.03.01" },
   { title: "요리가격 업데이트", date: "2025.02.27" },
   { title: "정기 업데이트", date: "2025.02.25" },
-  { title: "요리가격 업데이트", date: "2025.02.24" },
 
 ]
